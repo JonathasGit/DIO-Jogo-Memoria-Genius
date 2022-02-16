c078980443cc58f9c80d17c1d9fa55fde1978a3c
JonathasGit <jonathas-25@hotmail.com>
2022-02-16 17:55:13 -0200

    #1 parte 3

--- script.js
+++ script.js
@@ -17,7 +17,21 @@ let selecionarOrdem = () => { //Criando a função
     let guardarNumero = Math.floor(Math.random() * 4 ); // guarda numero aleatorio a cada rodada - Arredonda o número associado 
     ordem[ordem.length] = guardarNumero; // atribuir a ordem ao proximo numero ,
     ordemClicada = []; //Atribui o indice do arrary com a cor que sai de sorteio
-     
 
+    // Criar o for para percorrer o array e executar a função, para acender a cor 
+    for(let i in ordem){
+        let elementoCor = criarElemento(ordem[i]);
+        corClara(elementoCor, NUmber(i) + 1);
+    }   
+}
 
+let corClara = (Element, Number) => {
+    Number = Number * 500;
+    setTimeout(() => { // espera  o tempo passar
+        Element.classList.add('selected'); // vai add uma classe no selected 
+
+    }, Number - 250);
+    setTimeout(() =>{
+        Element.classList.remove('selected');
+    });
 }
